a7e6d191484b5d82c6fd3df0167e41093ffd9dd5
Neli Shahapuni <nneliswork@gmail.com>
2024-05-10 13:14:23 +0300

    [Task 0] Create Project + Import Icon & Assets

---
+++ Deliverfly/Deliverfly/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Deliverfly
+//
+//  Created by Neli Shahapuni on 5/10/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Deliverfly/Deliverfly/DeliverflyApp.swift
@@ -0,0 +1,17 @@
+//
+//  DeliverflyApp.swift
+//  Deliverfly
+//
+//  Created by Neli Shahapuni on 5/10/24.
+//
+
+import SwiftUI
+
+@main
+struct DeliverflyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
